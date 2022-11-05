667753b9443d8414757c88aee1f8fb93c45c4b82
Dmitriy Soymchenko <d.soymchenko@gmail.com>
2022-11-05 20:59:08 +0300

    feat: add burger menu function closing menu

--- js/script.js
+++ js/script.js
@@ -3,25 +3,33 @@
 let parent = document.querySelector(".slide_navigation");
 let labelItem = parent.querySelectorAll(".bar");
 parent.addEventListener("click", (event) => {
-  // Отлавливаем элемент в родители на который мы нажали
   let target = event.target;
 
-  // Проверяем тот ли это элемент который нам нужен
-  if (target.classList.contains("bar")) {
+  if (event.target.classList.contains("bar")) {
     for (let i = 0; i < labelItem.length; i++) {
-      // Убираем у других
       labelItem[i].classList.remove("activelabel");
     }
-    // Добавляем тому на который нажали
-    target.classList.add("activelabel");
+
+    event.target.classList.add("activelabel");
   }
 });
 
 /*----menu burger------*/
 const menuBurgerActive = document.querySelector(".burgermenu");
-console.log(menuBurgerActive);
+
 const naveActive = document.querySelector(".header__container");
+
 menuBurgerActive.addEventListener("click", function () {
   menuBurgerActive.classList.toggle("active");
   naveActive.classList.toggle("active");
+});
+
+naveActive.addEventListener("click", function (event) {
+  if (
+    event.target.closest(".header__container_item") &&
+    window.innerWidth < 768
+  ) {
+    menuBurgerActive.classList.remove("active");
+    naveActive.classList.remove("active");
+  }
 });
