690482043118a0d2fa3ef2c5cd5093e583e2ceaa
Dmitriy Soymchenko <d.soymchenko@gmail.com>
2022-11-28 12:19:38 +0300

    refactor: change menu burger closing

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
 /*--- active label----*/
 
-let parent = document.querySelector(".slide__navigation");
-let labelItem = parent.querySelectorAll(".bar");
-parent.addEventListener("click", (event) => {
-  if (event.target.classList.contains("bar")) {
-    for (let i = 0; i < labelItem.length; i++) {
-      labelItem[i].classList.remove("activelabel");
-    }
+let labelItem = document.querySelectorAll(".bar");
 
-    event.target.classList.add("activelabel");
-  }
-});
+for (const item of labelItem) {
+  item.addEventListener("click", function () {
+    for (const el of labelItem) {
+      el.classList.remove("activelabel");
+    }
+    this.classList.add("activelabel");
+  });
+}
 
 /*----menu burger------*/
 const menuBurgerActive = document.querySelector(".burgermenu");
-
 const naveActive = document.querySelector(".header__menu");
 
 menuBurgerActive.addEventListener("click", function () {
@@ -23,7 +21,7 @@ menuBurgerActive.addEventListener("click", function () {
 });
 
 naveActive.addEventListener("click", function (event) {
-  if (event.target.closest(".header__menu--item") && window.innerWidth < 768) {
+  if (event.target.closest(".header__menu") && window.innerWidth < 768) {
     menuBurgerActive.classList.remove("active");
     naveActive.classList.remove("active");
   }
